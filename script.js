ff33747b5d697e8163df04089df75998c9ffb0de
abdullah3034 <abdullahishak3034@gmail.com>
2024-06-07 21:30:48 +0530

    cal

---
+++ script.js
@@ -0,0 +1,42 @@
+
+function add(){
+
+let n1=document.getElementById("txt1").value;
+let n2=document.getElementById("txt2").value;
+n1=parseInt(n1);
+n2=parseInt(n2);
+alert("Total is " +(n1+n2));
+
+}
+
+function sub(){
+
+    let n1=document.getElementById("txt1").value;
+    let n2=document.getElementById("txt2").value;
+    n1=parseInt(n1);
+    n2=parseInt(n2);
+    alert("Total is " + (n1-n2));
+    
+    }
+    function mul(){
+
+        let n1=document.getElementById("txt1").value;
+        let n2=document.getElementById("txt2").value;
+        n1=parseInt(n1);
+        n2=parseInt(n2);
+        alert("Total is " + (n1*n2));
+        
+        }
+
+        function div(){
+
+            let n1=document.getElementById("txt1").value;
+            let n2=document.getElementById("txt2").value;
+            n1=parseInt(n1);
+            n2=parseInt(n2);
+            alert("Total is " + (n1/n2));
+            
+            }
+
+
+    
